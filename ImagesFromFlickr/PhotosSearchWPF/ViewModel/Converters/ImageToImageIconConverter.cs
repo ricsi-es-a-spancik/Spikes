b7fc8e854abac1a865a5227c2dfe04c24ec02726
David Hornyak <davidhornyak92@gmail.com>
2016-10-11 16:45:08 +0200

    List images in the library view with a corresponding icon to their existence on disk.

---
+++ ImagesFromFlickr/PhotosSearchWPF/ViewModel/Converters/ImageToImageIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PhotosSearchWPF.ViewModel.Converters
+{
+    public class ImageToImageIconConverter : IValueConverter
+    {
+        private static string ImageIconPath = @"../Images/Icons/Image_16x.png";
+        private static string ImageWarningIconPath = @"../Images/Icons/ImageWarning_16x.png";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string uriPath = (bool)value ? ImageIconPath : ImageWarningIconPath;
+            return new BitmapImage(new Uri(uriPath, UriKind.Relative));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
