93eff0f780f37fa9ed4cff754ee49ad8342a61b5
David Hornyak <davidhornyak92@gmail.com>
2016-10-30 16:43:11 +0100

    Add empty PRISM 6.2.0 WPF solution.

---
+++ NavigationWithPRISM/src/NavigationWithPRISM.Desktop/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace NavigationWithPRISM.Desktop
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
