4ed0e7d58c7eedeb8712987a20148bbf933eed0c
David Hornyak <davidhornyak92@gmail.com>
2016-10-31 19:43:40 +0100

    Using the navigation journal.

--- NavigationWithPRISM/LocalEntities/ViewModel/LocalItemDetailsViewModel.cs
+++ NavigationWithPRISM/LocalEntities/ViewModel/LocalItemDetailsViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using NavigationWithPRISM.Infrastructure;
 using Prism.Regions;
 using Business;
+using System.Windows.Input;
+using Prism.Commands;
 
 namespace LocalEntities.ViewModel
 {
     public class LocalItemDetailsViewModel : BindableBase, INavigationAware
     {
+        IRegionNavigationJournal _navigationJournal;
         private LocalItem _localItem;
 
         public LocalItem LocalItem
         {
             get { return _localItem; }
             set { SetProperty(ref _localItem, value); }
+        }
+
+        public ICommand NavigateBack { get; private set; }
+
+        public LocalItemDetailsViewModel()
+        {
+            NavigateBack = new DelegateCommand(OnNavigateBack, () => _navigationJournal?.CanGoBack ?? false);
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -25,11 +35,18 @@ namespace LocalEntities.ViewModel
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+            _navigationJournal = navigationContext.NavigationService.Journal;
+
             var localItemReceived = navigationContext.Parameters["LocalItem"] as LocalItem;
             if (localItemReceived != null)
             {
                 LocalItem = localItemReceived;
             }
+        }
+
+        private void OnNavigateBack()
+        {
+            _navigationJournal.GoBack();
         }
     }
 }
--- NavigationWithPRISM/src/Modules/RemoteSearch/ViewModel/RemoteItemDetailsViewModel.cs
+++ NavigationWithPRISM/src/Modules/RemoteSearch/ViewModel/RemoteItemDetailsViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using Business;
 using NavigationWithPRISM.Infrastructure;
+using Prism.Commands;
 using Prism.Regions;
+using System.Windows.Input;
 
 namespace RemoteSearch.ViewModel
 {
     public class RemoteItemDetailsViewModel : BindableBase, INavigationAware
     {
+        IRegionNavigationJournal _navigationJournal;
         private RemoteItem _remoteItem;
 
         public RemoteItem RemoteItem
         {
             get { return _remoteItem; }
             set { SetProperty(ref _remoteItem, value); }
+        }
+
+        public ICommand NavigateBack { get; private set; }
+
+        public RemoteItemDetailsViewModel()
+        {
+            NavigateBack = new DelegateCommand(OnNavigateBack, () => _navigationJournal?.CanGoBack ?? false);
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -25,11 +35,18 @@ namespace RemoteSearch.ViewModel
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+            _navigationJournal = navigationContext.NavigationService.Journal;
+
             var remoteItemReceived = navigationContext.Parameters["RemoteItem"] as RemoteItem;
             if (remoteItemReceived != null)
             {
                 RemoteItem = remoteItemReceived;
             }
+        }
+
+        private void OnNavigateBack()
+        {
+            _navigationJournal.GoBack();
         }
     }
 }
