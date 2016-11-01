7323e69590abf12a453668ad79dbce63f910f3de
David Hornyak <davidhornyak92@gmail.com>
2016-11-01 12:25:38 +0100

    Add custom FlickrNet DLL to Libs folder and use Styles during search.

--- ImagesFromFlickr/PhotosSearchWPF/ViewModel/SearchOptionsViewModel.cs
+++ ImagesFromFlickr/PhotosSearchWPF/ViewModel/SearchOptionsViewModel.cs
@@ -2,6 +2,11 @@
 using FlickrNet;
 using Prism.Events;
 using PhotosSearchWPF.ViewModel.Events;
+using System.Collections.ObjectModel;
+using System;
+using System.Linq;
+using System.Windows.Input;
+using System.Collections.Generic;
 
 namespace PhotosSearchWPF.ViewModel
 {
@@ -9,6 +14,7 @@ namespace PhotosSearchWPF.ViewModel
     {
         private const int PHOTOS_PER_PAGE = 20;
         private IEventAggregator _eventAggregator;
+        private HashSet<Style> _stylesIncluded = new HashSet<Style>();
 
         private string _searchCriteria;
 
@@ -38,13 +44,20 @@ namespace PhotosSearchWPF.ViewModel
             set { SetProperty(ref _searchByTags, value); }
         }
 
+        public ObservableCollection<Style> Styles { get; private set; }
+
         public DelegateCommand Search { get; private set; }
+
+        public ICommand AddStyle { get; private set; }
 
         public SearchOptionsViewModel(IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+
             SearchByText = true;
+            Styles = new ObservableCollection<Style>(Enum.GetValues(typeof(Style)).Cast<Style>());
             Search = new DelegateCommand(OnSearch, () => !string.IsNullOrEmpty(SearchCriteria));
+            AddStyle = new DelegateCommand<Style?>(OnAddStyle);
         }
 
         private void OnSearch()
@@ -54,6 +67,7 @@ namespace PhotosSearchWPF.ViewModel
                 PerPage = PHOTOS_PER_PAGE,
                 Page = 1,
                 SortOrder = PhotoSearchSortOrder.Relevance,
+                Styles = _stylesIncluded,
                 Extras = PhotoSearchExtras.All
             };
 
@@ -63,6 +77,17 @@ namespace PhotosSearchWPF.ViewModel
                 options.Tags = SearchCriteria;
 
             _eventAggregator.GetEvent<PhotoSearchRequested>().Publish(options);
+        }
+
+        private void OnAddStyle(Style? style)
+        {
+            if (!style.HasValue)
+                return;
+
+            if (_stylesIncluded.Contains(style.Value))
+                _stylesIncluded.Remove(style.Value);
+            else
+                _stylesIncluded.Add(style.Value);
         }
     }
 }
