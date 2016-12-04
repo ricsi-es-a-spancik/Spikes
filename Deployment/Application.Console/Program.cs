a7ec382151137a78450f6edb89b17ae54c3f84c7
David Hornyak <davidhornyak92@gmail.com>
2016-12-04 15:40:19 +0100

    Add a console application with a GUI-less wix installer.

---
+++ Deployment/Application.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadLine();
+        }
+    }
+}
