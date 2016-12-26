81fe2dff7225545f650fd9757e917590275e6c51
David Hornyak <davidhornyak92@gmail.com>
2016-12-26 14:02:30 +0100

    Add a WCF service project and install it along with the application as a Windows Service.

---
+++ Deployment/Service.Implementation/HoneyServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using Service.Interfaces;
+
+namespace Service.Implementation
+{
+    public class HoneyServiceImpl : IHoneyService
+    {
+        public string Echo(string message)
+        {
+            return message;
+        }
+
+        public ImageFileDTO GetImageFile(string path)
+        {
+            return new ImageFileDTO
+            {
+                Width = 0,
+                Height = 0,
+                NumberOfChannnels = 0,
+                Data = new byte[] { }
+            };
+        }
+    }
+}
---
+++ Deployment/Service.Interfaces/ImageFileDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Service.Interfaces
+{
+    [DataContract]
+    public class ImageFileDTO
+    {
+        [DataMember]
+        public int Width { get; set; }
+
+        [DataMember]
+        public int Height { get; set; }
+
+        [DataMember]
+        public int NumberOfChannnels { get; set; }
+
+        [DataMember]
+        public byte[] Data { get; set; }
+    }
+}
